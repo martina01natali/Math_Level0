63b2be49216de13af3e0b73092a01c9c2ce2f290
Daniele Zambelli <daniele.zambelli@gmail.com>
2018-09-07 22:42:25 +0200

    Sistemata la prima parte delle funzioni2.

--- deposito/materiali/12_Analisi/01_Topologia_Funzioni/funzioni02/src/09_funzioni.py
+++ deposito/materiali/12_Analisi/01_Topologia_Funzioni/funzioni02/src/09_funzioni.py
@@ -5,37 +5,38 @@ def f_1(x):
 tabella_o(f_1, range(-6, +6))
 
 def f_2(x):
-  """Restituisce la metà di x aumentata di 3."""
+  """Restituisce la meta' 
+  di x aumentata di 3."""
   return x / 2 + 3
 
 tabella_o(f_2, range(-8, +7))
 
 def f_3(x):
-  """Restituisce il doppio di x aumentato di 3."""
-  return 
+  """Restituisce il doppio 
+  di x aumentato di 3."""
+  return 2 * x + 3
 
 tabella_o(f_3, range(-8, +7))
 
 def f_4(x):
-  """Restituisce l'opposto di x aumentato di 3."""
-  return 
+  """Restituisce l'opposto 
+  di x aumentato di 3."""
+  return -x + 3
 
 tabella_o(f_4, range(-8, +7))
 
 def f_5(x):
-  """Restituisce l'opposto della meta' di x aumentato di 3."""
-  return 
+  """Restituisce l'opposto 
+  della meta' di x 
+  aumentata di 3."""
+  return -x / 2 + 3
 
 tabella_o(f_3, range(-8, +7))
 
 def f_6(x):
-  """Restituisce l'opposto del doppio di x aumentato di 3."""
-  return 
-
-tabella_o(f_3, range(-8, +7))
-
-def f_6(x):
-  """Restituisce l'opposto del doppio di x aumentato di 3."""
+  """Restituisce l'opposto 
+  del doppio di x 
+  aumentato di 3."""
   return -2 * x +3
 
 tabella_o(f_6, range(-4, +7))
